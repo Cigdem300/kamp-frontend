d7c83fd3854415207fba3cb0476e38f1bcef338b
Çiğdem Tayfur <cigdemtayfur00@outlook.com>
2021-03-18 16:13:23 +0300

    18MART2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import {HttpClient} from '@angular/common/http'
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 //axios,fetch
 
@@ -11,20 +10,23 @@ import { ProductResponseModel } from 'src/app/models/productResponseModel';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  products:Product[] = [];
+  dataLoaded = false;
+  
+  constructor(private productService:ProductService) { }
+
+ngOnInit(): void {
+  this.getProducts();
+}
+getProducts(){
+  this.productService.getProducts().subscribe(response=> {
+    this.products=response.data
+    this.dataLoaded=true;
+  }) 
+  
+}
+}
 
   
-  products:Product[] = [];
-  apiUrl="https://localhost:44395/api/products/getall"
-    constructor(private httpClient:HttpClient) { }
+ 
 
-  ngOnInit(): void {
-    this.getProducts();
-  }
-  getProducts(){
-    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
-      this.products=response.data
-    });
-
-  }
-
-}
