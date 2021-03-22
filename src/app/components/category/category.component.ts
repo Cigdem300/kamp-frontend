2315ab97bf2a8e95258f90dbcf70ba45576992e4
Çiğdem Tayfur <cigdemtayfur00@outlook.com>
2021-03-22 15:00:22 +0300

    22mart2021-19.gün

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -42,4 +42,14 @@ export class CategoryComponent implements OnInit {
 
   }
 
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+      return "list-group-item active"
+    }
+    else {
+      return "list-group-item "
+    }
+    
+  }
+
 }
